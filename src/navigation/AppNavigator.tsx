1176e83425d98a021b994355bc0ca51307a7c926
sachinbhoir4u <sachinbhoir4u@gmail.com>
2025-09-17 20:21:49 +0530

    AppNavigator updated

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,37 +1,3 @@
-// import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import Login from '../screens/Login';
-// import Registration from '../screens/Registration';
-// import { COLORS } from '../utils/constants';
-
-// type RootStackParamList = {
-//   Login: undefined;
-//   Register: undefined;
-//   Dashboard: undefined;
-// };
-
-// const Stack = createStackNavigator<RootStackParamList>();
-
-// export default function AppNavigator() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator
-//         initialRouteName="Login"
-//         screenOptions={{
-//           headerStyle: { backgroundColor: COLORS.primary },
-//           headerTintColor: '#fff',
-//           headerTitleStyle: { fontWeight: 'bold' },
-//         }}
-//       >
-//         <Stack.Screen name="Login" component={Login} options={{ title: 'Society App' }} />
-//         <Stack.Screen name="Register" component={Registration} options={{ title: 'Register' }} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
